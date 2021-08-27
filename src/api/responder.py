2f71bbb2e771fc530d9234025941d4e5c2622676
Stephen <stephen_archbold@msn.com>
2021-08-27 16:01:47 -0400

    Formatted with black

--- src/api/responder.py
+++ src/api/responder.py
@@ -8,6 +8,7 @@ app = FastAPI()
 async def root():
     return {"message": "Execute query by adding /countries/country=usa"}
 
+
 @app.get("/countries/")
 async def return_list(country: str):
     results = getsupportedstates(country)
