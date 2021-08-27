ecc7030516ddff18376be5dd4282f6f2e0a187b8
Stephen <stephen_archbold@msn.com>
2021-08-27 15:05:22 -0400

    Added DF output for API return

--- src/api/request.py
+++ src/api/request.py
@@ -1,21 +1,27 @@
 """
 Send the request to the external API and return results
 """
-
+import pandas as pd
+from urllib.request import urlopen
 import requests
+import json
 
-def getSupportedStates(country):
+def getsupportedstates(country):
     req = (
         "http://api.airvisual.com/v2/states?country="
         + country
         + "&key=14098954-5274-4969-b70a-c68fc6d522ca"
     )
-    response = requests.get(req)
-    print(response.text)
-    print(req)
+    response = urlopen(req)
+    json_data = response.read().decode('utf-8', 'replace')
+
+    d = json.loads(json_data)
+    df = pd.json_normalize(d['data'])
+    print(df)
 
 
-def getCountryData(city, state, country):
+
+def getcountrydata(city, state, country):
     req = (
         "http://api.airvisual.com/v2/city?city="
         + city
@@ -25,6 +31,9 @@ def getCountryData(city, state, country):
         + country
         + "&key=14098954-5274-4969-b70a-c68fc6d522ca"
     )
-    response = requests.get(req)
-    print(response.text)
-    print(req)
+    response = urlopen(req)
+    json_data = response.read().decode('utf-8', 'replace')
+
+    d = json.loads(json_data)
+    df = pd.json_normalize(d['data'])
+    print(df)
