540ab4583807af853f2871b19530d5acf87802d8
Sarchbold <stephen_archbold@msn.com>
2021-08-28 12:58:37 -0400

    feat: Added country lookup (#25)
    * Made shell script executable
    
    * Adding query parameter to fastapi
    
    * Formatted files
    
    * Added additional paths for web call
    
    * Formatted with black
    
    * Bug fix query message
    
    * Addressed fastapi syntax error

--- src/api/request_tester.py
+++ src/api/request_tester.py
@@ -1,7 +1,15 @@
 """ Test area for functionality """
 
 from request import *
+import json
 
-getsupportedstates("USA")
+response = getsupportedstates("USA", "raw")
+print(response)
 
+response = getsupportedstates("USA", "json")
+print(response)
+
+# d = json.loads(response)
+# df = pd.json_normalize(d["data"])
+# print(df)
 # getCountryData('Boston', 'MA', 'USA')
--- src/api/responder.py
+++ src/api/responder.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from request import *
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Execute query by adding /countries/?country=usa"}
+
+
+@app.get("/countries/")
+async def return_list(country: str):
+    results = getsupportedstates(country)
+    return results
