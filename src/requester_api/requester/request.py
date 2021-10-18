066eed1ded5436fdbd79b19bebd4b837b69b0494
Stephen <stephen_archbold@msn.com>
2021-10-17 20:25:39 -0400

    Restructured project

--- src/requester_api/requester/request.py
+++ src/requester_api/requester/request.py
@@ -14,23 +14,8 @@ class Requester:
         req = (
                 "http://api.airvisual.com/v2/states?country="
                 + self.country
-                + "&key=14098954-5274-4969-b70a-c68fc6d522ca"
+                + "&key=<getkey>"
         )
         response = urlopen(req)
         json_data = response.read().decode("utf-8", "replace")
         return json_data
-
-
-    # def getcountrydata(city, state, country):
-    #     req = (
-    #             "http://api.airvisual.com/v2/city?city="
-    #             + city
-    #             + "&state="
-    #             + state
-    #             + "&country="
-    #             + country
-    #             + "&key=14098954-5274-4969-b70a-c68fc6d522ca"
-    #     )
-    #     response = urlopen(req)
-    #     json_data = response.read().decode("utf-8", "replace")
-    #     return json_data
